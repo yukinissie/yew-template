0064e66a8371f7a3c094b33d234bb09ce1b25351
yukinissie <bigtree2490@outlook.jp>
2022-01-29 17:44:44 +0900

    AzureへデプロイするためのCI/CDを追加

--- src/app.rs
+++ src/app.rs
@@ -5,7 +5,7 @@ pub fn app() -> Html {
     html! {
         <main>
             <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World from Azure!" }</h1>
+            <h1>{ "Hello World!" }</h1>
             <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
         </main>
     }
